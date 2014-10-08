a351c3e6c61cc3e9e4a95962f968c0d68c02543b
SrikarSky <srikarsky@gmail.com>
2014-10-07 21:36:33 -0700

    C++ Progs
    Hello World in C++

---
+++ CPP/SimpleCPPHelloWorld.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  CPPProgs
+//
+//  Created by Srikar Surendra on 10/6/14.
+//  Copyright (c) 2014 CMagic. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>  //for std::vector
+#include <string>  //for std::string
+using std::vector;
+using std::string;
+/* Dont use lists if you can help it */
+/*
+ 
+ using std::list;
+ using std::string;
+ list<string> myList;
+ myList.push_back("Hello");
+ */
+
+int main(int argc, const char * argv[])
+{
+    
+    std::cout << "Hello, World!\n";
+    
+    vector<string> data;
+    data.push_back("my name");
+    
+    
+    return 0;
+}
+
